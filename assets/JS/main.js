274e87dc56e7394f47c2606619adc295d6fd9f25
andhip <andhi.puspianto10@gmail.com>
2022-01-14 18:40:22 +0700

    modified zoomVwr

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -23,4 +23,17 @@
     );
     return false;
   });
+
+  // ------------------------------------------------------------------------------ //
+  // Protfolio Viewer
+  // ---------------------------------------------------------
+  $(".details_gall_image .responsive-image").click(function () {
+    $("#full-image").attr("src", $(this).attr("src"));
+    $("#image-viewer").show();
+  });
+
+  $("#image-viewer .close").click(function () {
+    $("#image-viewer").hide();
+  });
+  console.log("image-viewer active");
 })(jQuery);
