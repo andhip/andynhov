a882271c43fabcb9751d0730783b1eec43749142
andhip <andhi.puspianto10@gmail.com>
2022-10-12 14:42:17 +0700

    modified styles

--- assets/JS/switch.js
+++ assets/JS/switch.js
@@ -1,36 +1,36 @@
-const themeToggle = document.querySelector("#switch-theme");
+// const themeToggle = document.querySelector("#switch-theme");
 
-themeToggle.addEventListener("click", () => {
-  document.body.classList.contains("light-theme")
-    ? enableDarkMode()
-    : enableLightMode();
+// themeToggle.addEventListener("click", () => {
+//   document.body.classList.contains("light-theme")
+//     ? enableDarkMode()
+//     : enableLightMode();
 
-  console.log("theme active");
-});
+//   console.log("theme active");
+// });
 
-function enableDarkMode() {
-  document.body.classList.remove("light-theme");
-  document.body.classList.add("dark-theme");
-  themeToggle.setAttribute("aria-label", "Switch to light theme");
-}
+// function enableDarkMode() {
+//   document.body.classList.remove("light-theme");
+//   document.body.classList.add("dark-theme");
+//   themeToggle.setAttribute("aria-label", "Switch to light theme");
+// }
 
-function enableLightMode() {
-  document.body.classList.remove("dark-theme");
-  document.body.classList.add("light-theme");
-  themeToggle.setAttribute("aria-label", "Switch to dark theme");
-  let arrow = document.getElementById("arrow");
-  arrow.style.fill = "fill(" + " #0f0f0f)";
-}
+// function enableLightMode() {
+//   document.body.classList.remove("dark-theme");
+//   document.body.classList.add("light-theme");
+//   themeToggle.setAttribute("aria-label", "Switch to dark theme");
+//   let arrow = document.getElementById("arrow");
+//   arrow.style.fill = "fill(" + " #0f0f0f)";
+// }
 
-function setThemePreference() {
-  if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-    enableDarkMode();
-    return;
-  }
-  enableLightMode();
-}
+// function setThemePreference() {
+//   if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+//     enableDarkMode();
+//     return;
+//   }
+//   enableLightMode();
+// }
 
-document.onload = setThemePreference();
+// document.onload = setThemePreference();
 
 // scroll rotate
 window.onscroll = function () {
